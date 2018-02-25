e977e6465ae975b53a30ba27fb235077327fea9d
Mirek <djmirq@wp.pl>
2018-02-25 09:49:49 +0100

    Rozbudowanie klasy osoba

--- src/hello/Osoba.java
+++ src/hello/Osoba.java
@@ -1,6 +1,37 @@
 package hello;
 
 public class Osoba {
-	String imie, nazwisko;
+	private String imie, nazwisko;
 
+	public String getImie() {
+		return imie;
+	}
+
+	public void setImie(String imie) {
+		this.imie = imie;
+	}
+
+	public String getNazwisko() {
+		return nazwisko;
+	}
+
+	public void setNazwisko(String nazwisko) {
+		this.nazwisko = nazwisko;
+	}
+
+	public Osoba(String imie, String nazwisko) {
+		super();
+		this.imie = imie;
+		this.nazwisko = nazwisko;
+	}
+
+	@Override
+	public String toString() {
+		return "Osoba [imie=" + imie + ", nazwisko=" + nazwisko + ", getImie()=" + getImie() + ", getNazwisko()="
+				+ getNazwisko() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
+	}
+
+	
+	
 }
